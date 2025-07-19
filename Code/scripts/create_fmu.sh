88aefe1aa699631245b50c99b9f81843c9798e5e
Fitz Koch <fw.j.keenan.koch@gmail.com>
2025-07-19 12:22:39 -0400

    updated readme

--- Code/scripts/create_fmu.sh
+++
@@ -1,9 +0,0 @@
-fmipp_create_fmu \
-    --model-name PowerSink \
-    --python-class PowerSink \
-    --python-file PowerSink.py \
-    --fmi-version 2 \
-    --interface-type CoSimulation \
-    --description "Power input integrator" \
-    --output-dir PowerSink_fmu \
-    --model-description modelDescription.xml.in
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # lca-fmu
 
+The python fmus are build using syntax like `pythonfmu build -f power_sink.py` from within the `powersink_fmu` syntax. Needs refinement, though.
 
 # Credits
 Code written by  Fitz Koch: fkeenank@uvm.edu
